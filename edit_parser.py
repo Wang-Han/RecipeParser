02511bfe478d3c828f1a860f6a5f514ab217a0c7
noradiegwu <nneomaoradiegwu2019@u.northwestern.edu>
2018-03-14 14:01:34 -0500

    parse_ingredients

--- edit_parser.py
+++ edit_parser.py
@@ -1,44 +1,22 @@
 from get_ingredients_from_bbc import get_ingredients
 from scraper import scrapeRecipe
-# from ingredients_parser import *
 import re
 from pattern.en import pluralize
-
-# print pluralize("tomato")
-
-# "allingredients"
-
-# print all_ing_list
-
-# import ast
-# x = ast.literal_eval(all_ingredients)
-#
-# for g in x:
-#     print g
-
-
 
 
 # all_ing = get_ingredients()
 # print len(all_ing)
 print "done"
-recipe = scrapeRecipe("https://www.allrecipes.com/recipe/21412/tiramisu-ii/?clickId=right%20rail0&internalSource=rr_feed_recipe_sb&referringId=247082%20referringContentType%3Drecipe")
-ings = recipe[0]
-steps = recipe[1]
-print ings, '\n' #, steps, '\n'
-
-# TODO:
-# ADD: tomato sauce, cooking spray, tortillas, yellow squash, zucchini, tortillas, bell pepper,
-# REMOVE: fat,
-# make subs for healthy, veg, chinese,
-# make better ingredient parser
+# recipe = scrapeRecipe("https://www.allrecipes.com/recipe/21412/tiramisu-ii/?clickId=right%20rail0&internalSource=rr_feed_recipe_sb&referringId=247082%20referringContentType%3Drecipe")
+# ings = recipe[0]
+# steps = recipe[1]
+# print ings, '\n' #, steps, '\n'
 
 
 # lstlst = []
 # for i in ings: # from scraper
 #     name_ret = get_ing_name(i)
 #     lstlst.append(name_ret)
-
 
 
 # stuff in front == qty, DESCRIPTOR
@@ -72,7 +50,7 @@ def bbc_ingredients_from_txt():
                 t.append(true_ing.lower())
             elif match_plural:
                 t.append(pluralize(true_ing.lower()))
-        print "HEREHEREHEREHEREHEREHERE:", t
+        # print "HEREHEREHEREHEREHEREHERE:", t
 
 
 '''
@@ -82,6 +60,7 @@ def get_all_names(url):
     #scrape
     recipe = scrapeRecipe(url)
     ings = recipe[0]
+    ings = map(lambda x:x.lower(), ings)
 
     # get bbc ingredients
     all_ingredients = open("allingredients.txt", "r")
@@ -104,7 +83,6 @@ def get_all_names(url):
             t.append(true_ing.lower())
         elif match_plural:
             t.append(pluralize(true_ing.lower()))
-    print "HEREHEREHEREHEREHEREHERE:", t
 
     seen = set()
     seen_add = seen.add
@@ -112,37 +90,23 @@ def get_all_names(url):
 
     t.sort(key=lambda x: len(x.split()), reverse=True)
 
-    print t
+    # print t
 
     names = []
     desc_and_prep = []
 
     name = ""
-    for i in ingredients:
+
+    for i in ings:
         for db_ingredient in t:
             if db_ingredient in i.lower():
-                print "HERE:", db_ingredient
+                # print "HERE:", db_ingredient
                 names.append(db_ingredient)
                 # db_ingredient is now the name of the Ingredient
                 desc_and_prep.append(i.split(db_ingredient))
                 break
 
     return [names, desc_and_prep] #[ ['white sugar'], [[u'1 cup', u'']]]
-
-
-
-# class Ingredient:
-#     def __init__(self, ing_string):
-#         self.str = ing_string
-#         self.name = ""
-#         self.quantity = 0
-#         self.measurement = ""
-#         self.descriptor = ""
-#         self.preparation = ""
-#         # self.name_with_descriptor = ""
-#
-#         # def make_ingredients():
-#         #     return lst_of_ingredients
 
 def get_ing_quantity(ing_front):
     ingredient_split_list = ing_front.split()
@@ -162,34 +126,98 @@ def get_ing_measurement(ing_front):
         measurement = i if re.search(r"(?=("+'|'.join(measurements)+r"))", i) else ""
         regex = re.compile(r'[^a-zA-Z]')
         regex.sub('', measurement)
-    return measurement
+    return measurement.lower()
 
 def get_ing_descriptor(ing_front):
     ingredient_split_list = ing_front.split()
     descriptor = ""
     no_nums_ingredient = [w for w in ingredient_split_list if not re.search(fraction_match, w)]
-    m = get_ing_measurement(ing_front)
+    no_nums_ingredient = map(lambda x:x.lower(), no_nums_ingredient)
+    m = get_ing_measurement(ing_front).lower()
     descriptor = ' '.join(no_nums_ingredient).replace(m, '').strip()
-    return descriptor
+    return descriptor.strip()
 
 def get_ing_preparation(ing_back):
     return ing_back.strip()
 
 
-def print_ingredients(all_names_results): # substitution -> "vegan", "greek", etc. --> name = name[x].greek: figure out how to incorporate IngredientBook
-    all_results = get_all_names(ings)
+'''
+input: allrecipes url
+output: prints -->
+N: parmesan cheese
+Q: 0.25
+M: cup
+D: grated
+P:
+"
+'''
+def print_ingredients(url): # substitution -> "vegan", "greek", etc. --> name = name[x].greek: figure out how to incorporate IngredientBook
+    recipe = scrapeRecipe(url)
+    ings = recipe[0]
+    all_results = get_all_names(url)
     names = all_results[0]
-    other = all_results[1]
+    desc_and_preps = all_results[1]
     for x in range(len(names)):
-        # b = Ingredient(i)
         name = names[x]
-        print name
-        qty = get_ing_quantity(other[x][0])
-        print qty
-        msrmnt = get_ing_measurement(other[x][0])
-        print msrmnt
-        desc = get_ing_descriptor(other[x][0])
-        print desc
-        if len(other) > 1:
-            prep = get_ing_preparation(other[x][1])
-            print prep
+        print "N:", name
+        qty = get_ing_quantity(desc_and_preps[x][0])
+        print "Q:", qty
+        msrmnt = get_ing_measurement(desc_and_preps[x][0])
+        print "M:", msrmnt
+        desc = get_ing_descriptor(desc_and_preps[x][0])
+        print "D: ", desc
+        if len(desc_and_preps[x]) > 1:
+            prep = get_ing_preparation(desc_and_preps[x][1])
+            print "P:", prep
+        print '\n'
+
+'''
+input: allrecipes url
+output:
+{'olive oil': {'preparation': u'', 'descriptor': u'', 'measurement': u'tablespoon', 'name': 'olive oil', 'quantity': 1.0}, 'parmesan cheese': {'preparation': u'', 'descriptor'
+: u'grated', 'measurement': u'cup', 'name': 'parmesan cheese', 'quantity': 0.25}, etc.}
+use: ex. quantity of parmesan in : p["parmesan cheese"]["quantity"] --> 0.25
+'''
+
+def parse_ingredients(url):
+    recipe = scrapeRecipe(url)
+    ings = recipe[0]
+    all_results = get_all_names(url)
+    names = all_results[0]
+    desc_and_preps = all_results[1]
+
+    all_parsed_ings = {}
+
+
+    for x in range(len(names)):
+        parsed = {"name":"", "quantity":"", "measurement":"", "descriptor":"", "preparation":""}
+        name = names[x]
+        # print "N:", name
+        parsed["name"] = name
+        qty = get_ing_quantity(desc_and_preps[x][0])
+        # print "Q:", qty
+        parsed["quantity"] = qty
+        msrmnt = get_ing_measurement(desc_and_preps[x][0])
+        # print "M:", msrmnt
+        parsed["measurement"] = msrmnt
+        desc = get_ing_descriptor(desc_and_preps[x][0])
+        # print "D: ", desc
+        parsed["descriptor"] = desc
+        if len(desc_and_preps[x]) > 1:
+            prep = get_ing_preparation(desc_and_preps[x][1])
+            # print "P:", prep
+            parsed["preparation"] = prep
+        all_parsed_ings[name] = parsed
+    print all_parsed_ings
+    return all_parsed_ings
+
+# TODO: Find a way to distinguish same ingredient for different purposes
+# if descriptor is diff, make ame diff?
+# add a new property that can tell the difference
+
+my_url = "https://www.allrecipes.com/recipe/223042/chicken-parmesan/?internalSource=previously%20viewed&referringContentType=home%20page&clickId=cardslot%204"
+all_names = get_all_names(my_url)[0]
+print all_names
+print_ingredients(my_url)
+p = parse_ingredients(my_url)
+print p["parmesan cheese"]
